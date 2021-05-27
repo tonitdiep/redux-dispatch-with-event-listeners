4efcdcb87662d52dde4db50cc724c560d7ffad3b
tonitdiep <adiep123@gmail.com>
2021-05-27 01:35:23 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,23 @@
 // add code snippets from README
+let state;
+
+function changeState(state = { count: 0 }, action) {
+    switch(action.type) {
+        case 'INCREASE_COUNT':
+            return { count: state.count + 1 }
+        default:
+            return state;
+
+    }
+}
+function dispatch(action){
+        state = changeState(state, action)
+        render()
+    }
+
+function render(){
+        document.body.textContent = state.count
+    }
+
+dispatch({ type: '@@INIT' })
+
